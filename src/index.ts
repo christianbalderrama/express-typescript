795984460389d9c68d737f070ae0854490c64b40
Christian Balderrama <0christianbalderrama@gmail.com>
2022-06-23 21:55:23 +0800

    ✨ Implement pino-http and update npm scripts

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,18 @@
 import express, {Express, Request, Response} from 'express';
 import dotenv from 'dotenv';
-import pino from 'pino';
+import pinoHttp from 'pino-http';
 
 dotenv.config();
 
-const logger = pino();
 const app: Express = express();
 const port: string | number = process.env.PORT || 300;
 
+// Use pinoHttp middleware and logger and automatically binds to req.log
+app.use(pinoHttp());
+
 app.get('/', (req: Request, res: Response) => {
+  req.log.info({req, res});
   res.send('Express + TypeScript Server');
 });
 
-app.listen(port, () => {
-  logger.info({}, `⚡️ [server]: Server is running at https://localhost:${port}`);
-});
+app.listen(port, () => console.log(`⚡️ [server]: Server is running at https://localhost:${port}`));
