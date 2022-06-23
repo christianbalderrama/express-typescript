7b639c68e4a9181a749396362d2ba973ce5b633c
Christian Balderrama <0christianbalderrama@gmail.com>
2022-06-23 22:30:37 +0800

    🔧 Finalize logger options

--- src/index.ts
+++ src/index.ts
@@ -4,14 +4,26 @@ import pinoHttp from 'pino-http';
 
 dotenv.config();
 
+const logger = pinoHttp({
+  quietReqLogger: true, // turn off the default logging output
+  transport: {
+    target: 'pino-pretty', // use the pino-http-print transport and its formatting output
+    options: {
+      levelFirst: true, // [INFO | ERROR ] ...etc
+      colorize: true, // Add colors to the levels
+      crlf: true, // New line if string is too long
+      translateTime: true // translates epoch/unix time to human readable time
+    }
+  }
+});
 const app: Express = express();
 const port: string | number = process.env.PORT || 300;
 
 // Use pinoHttp middleware and logger and automatically binds to req.log
-app.use(pinoHttp());
+app.use(logger);
 
 app.get('/', (req: Request, res: Response) => {
-  req.log.info({req, res});
+  req.log.info({req, res}, "TESTSETSETSETESTs");
   res.send('Express + TypeScript Server');
 });
 
